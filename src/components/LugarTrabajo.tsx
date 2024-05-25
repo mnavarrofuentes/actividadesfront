c4d023592fc07ca56b121e50e077e4e5708366e4
Milton Navarro <mnavarro@sigel.gt>
2024-05-24 21:27:47 -0600

    guardar tareas en base de datos simple

--- src/components/LugarTrabajo.tsx
+++ src/components/LugarTrabajo.tsx
@@ -2,12 +2,27 @@ import React, { useState, useEffect, useRef } from "react";
 import { Container, Modal, Form, Button, ListGroup } from "react-bootstrap";
 import Navigation from "./Navbar";
 import { useNavigate } from "react-router-dom";
-
+import { jwtDecode } from "jwt-decode";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { toast, ToastContainer } from "react-toastify";
 interface Tarea {
   nombre: string;
   descripcion: string;
   fechaLimite: string;
 }
+
+const formatDate = (date: Date): string => {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+};
+
+const parseDate = (dateStr: string): Date => {
+  const [day, month, year] = dateStr.split("-").map(Number);
+  return new Date(year, month - 1, day);
+};
 
 const LugarTrabajo: React.FC = () => {
   const navigate = useNavigate();
@@ -17,7 +32,7 @@ const LugarTrabajo: React.FC = () => {
   const [newTarea, setNewTarea] = useState<Tarea>({
     nombre: "",
     descripcion: "",
-    fechaLimite: "",
+    fechaLimite: formatDate(new Date()),
   });
 
   useEffect(() => {
@@ -28,24 +43,95 @@ const LugarTrabajo: React.FC = () => {
         navigate("/login", { replace: true });
       } else {
         console.log("estoy dentro autenticado");
+        obtenerTareas(); // Llamada a la función para obtener tareas
       }
       initializedRef.current = true;
     }
   }, [navigate]);
 
+  const obtenerTareas = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.log("Token no encontrado");
+        return;
+      }
+      const decodedToken: any = jwtDecode(token);
+      const creadorId = decodedToken.userId;
+
+      const response = await fetch(
+        `https://localhost:32768/api/tareas/creador/${creadorId}`
+      );
+      if (!response.ok) {
+        throw new Error("Error al obtener las tareas");
+      }
+      const data = await response.json();
+      setTareas(data);
+    } catch (error) {
+      console.error("Error al obtener las tareas:", error);
+    }
+  };
+
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setNewTarea({ ...newTarea, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleDateChange = (date: Date | null) => {
+    if (date) {
+      const formattedDate = formatDate(date);
+      setNewTarea({ ...newTarea, fechaLimite: formattedDate });
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setTareas([...tareas, newTarea]);
-    setNewTarea({ nombre: "", descripcion: "", fechaLimite: "" });
-    handleCloseModal();
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.log("Token no encontrado");
+      return;
+    }
+
+    const decodedToken: any = jwtDecode(token);
+    const creadorId = decodedToken.userId; // ajusta esto según la estructura de tu token
+
+    const tareaConDatos = {
+      ...newTarea,
+      prioridad: 0,
+      completada: false,
+      creadorId: creadorId,
+    };
+
+    try {
+      const response = await fetch("https://localhost:32768/api/tareas", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(tareaConDatos),
+      });
+
+      if (response.ok) {
+        const nuevaTarea = await response.json();
+        setTareas([...tareas, nuevaTarea]);
+        setNewTarea({
+          nombre: "",
+          descripcion: "",
+          fechaLimite: formatDate(new Date()),
+        });
+        handleCloseModal();
+        toast.success("Usuario creado exitosamente");
+      } else {
+        console.error("Error al crear la tarea");
+      }
+    } catch (error) {
+      console.error("Error al conectar con la API", error);
+    }
   };
 
   return (
@@ -66,7 +152,7 @@ const LugarTrabajo: React.FC = () => {
                   type="text"
                   name="nombre"
                   value={newTarea.nombre}
-                  onChange={handleChange}
+                  onChange={handleInputChange}
                   required
                 />
               </Form.Group>
@@ -76,17 +162,17 @@ const LugarTrabajo: React.FC = () => {
                   type="text"
                   name="descripcion"
                   value={newTarea.descripcion}
-                  onChange={handleChange}
+                  onChange={handleInputChange}
                   required
                 />
               </Form.Group>
               <Form.Group controlId="formFechaLimite" className="mt-3">
                 <Form.Label>Fecha Límite</Form.Label>
-                <Form.Control
-                  type="date"
-                  name="fechaLimite"
-                  value={newTarea.fechaLimite}
-                  onChange={handleChange}
+                <DatePicker
+                  selected={parseDate(newTarea.fechaLimite)}
+                  onChange={handleDateChange}
+                  dateFormat="dd-MM-yyyy"
+                  className="form-control"
                   required
                 />
               </Form.Group>
